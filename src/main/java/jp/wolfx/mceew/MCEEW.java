7499f6f30d3a8fd17abd46185805e50d7ab55b2a
TenkyuChimata <tenkyuchimata@gmail.com>
2023-12-29 16:50:31 +0800

    Intensity support shows decimals

--- src/main/java/jp/wolfx/mceew/MCEEW.java
+++ src/main/java/jp/wolfx/mceew/MCEEW.java
@@ -116,7 +116,7 @@ public final class MCEEW extends JavaPlugin {
             String region = "四川广元市青川县";
             String mag = "3.2";
             String depth = "10km";
-            String intensity = "5";
+            String intensity = "5.0";
             String origin_time = getDate("yyyy-MM-dd HH:mm:ss", time_format, "Asia/Shanghai", origin_time_str);
             scEewAction(report_time, origin_time, num, lat, lon, region, mag, depth, getIntensityColor(intensity));
         } else if (flag == 3) {
@@ -169,16 +169,16 @@ public final class MCEEW extends JavaPlugin {
         }
     }
 
-    private void mceewScheduler(boolean updaterBoolean) {
+    private void mceewScheduler(boolean first) {
         if (!folia) {
-            Bukkit.getScheduler().runTaskAsynchronously(this, () -> wsClient(updaterBoolean));
-            if (updaterBoolean) {
+            Bukkit.getScheduler().runTaskAsynchronously(this, () -> wsClient(first));
+            if (first) {
                 Bukkit.getScheduler().runTaskAsynchronously(this, this::updater);
             }
         } else {
             Plugin plugin = this;
-            Bukkit.getAsyncScheduler().runNow(plugin, task1 -> wsClient(updaterBoolean));
-            if (updaterBoolean) {
+            Bukkit.getAsyncScheduler().runNow(plugin, task1 -> wsClient(first));
+            if (first) {
                 Bukkit.getAsyncScheduler().runNow(plugin, task2 -> updater());
             }
         }
@@ -399,13 +399,13 @@ public final class MCEEW extends JavaPlugin {
         String lon = scEewData.get("Longitude").getAsString();
         String region = scEewData.get("HypoCenter").getAsString();
         String mag = scEewData.get("Magunitude").getAsString();
+        String intensity = scEewData.get("MaxIntensity").getAsString();
         String depth;
         if (!scEewData.get("Depth").isJsonNull()) {
             depth = scEewData.get("Depth").getAsString() + "km";
         } else {
             depth = "10km";
         }
-        String intensity = String.valueOf(Math.round(Float.parseFloat(scEewData.get("MaxIntensity").getAsString())));
         String origin_time = getDate("yyyy-MM-dd HH:mm:ss", time_format, "Asia/Shanghai", scEewData.get("OriginTime").getAsString());
         scEewAction(report_time, origin_time, num, lat, lon, region, mag, depth, getIntensityColor(intensity));
     }
@@ -675,7 +675,11 @@ public final class MCEEW extends JavaPlugin {
 
     private static String getIntensityColor(String intensity) {
         String[] intensity_color = new String[]{"§f", "§7", "§b", "§3", "§9", "§a", "§2", "§e", "§6", "§c", "§4", "§d", "§5"};
-        return intensity_color[Integer.parseInt(intensity)] + intensity;
+        int index = Math.round(Float.parseFloat(intensity));
+        if (index >= intensity_color.length) {
+            index = intensity_color.length - 1;
+        }
+        return intensity_color[index] + intensity;
     }
 
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
